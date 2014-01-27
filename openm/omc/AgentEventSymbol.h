1f4956841e7e258c4aaff1ca1a4290cdb3ef5372
esseffgee <esseffgee@users.sf.net>
2014-01-27 06:21:00 +0000

    omc: Safety checkpoint

--- openm/omc/AgentEventSymbol.h
+++ openm/omc/AgentEventSymbol.h
@@ -1,62 +1,82 @@
 /**
-* @file    AgentEventSymbol.h
-* Declarations for the AgentEventSymbol class.
+* @file    AgentEventTimeSymbol.h
+* Declarations for the AgentEventTimeSymbol class.
 */
 // Copyright (c) 2013-2014 OpenM++
 // This code is licensed under MIT license (see LICENSE.txt for details)
 
 #pragma once
-#include "AgentMemberSymbol.h"
+#include "AgentCallbackMemberSymbol.h"
+#include "TypedefTypeSymbol.h"
 
-class CodeBlock;
+class AgentFuncSymbol;
 
 using namespace std;
 
-
 /**
- * Symbol for an Agentevent
- * 
- * An AgentEventSymbol is created when an 'event' statement is encountered in the model source
- * code.  The name of the symbol is the name of the event implement function specified in the
- * 'event' statement.
- */
+* Event time member symbol for an event.
+* Abstracts a member of an agent class which stores the event time
+* of an agent event.
+*/
 
-class AgentEventSymbol : public AgentMemberSymbol
+class AgentEventTimeSymbol : public AgentCallbackMemberSymbol
 {
 private:
-    typedef AgentMemberSymbol super;
+    typedef AgentCallbackMemberSymbol super;
 
 public:
     bool is_base_symbol() const { return false; }
 
-
-    /**
-    * Constructor.
-    *
-    * @param [in,out]  sym     The symbol to be morphed (event implement function)
-    * @param   agent           The agent.
-    * @param   time_function   The symbol for the associated event time function.
-    */
-
-    AgentEventSymbol(Symbol *sym, const Symbol *agent, const Symbol *time_function, yy::location decl_loc = yy::location())
-        : AgentMemberSymbol(sym, agent, decl_loc)
-//        , time_function(time_function->get_rpSymbol())
-//        , pp_event_id(0)
+    AgentEventTimeSymbol(const string event_name, const Symbol *agent, const Symbol *time_func, const Symbol *implement_func, yy::location decl_loc = yy::location())
+        : AgentCallbackMemberSymbol(event_name, agent, TypedefTypeSymbol::get_typedef_symbol(token::TK_Time), decl_loc)
+        , event_name(implement_func->name)
+        , time_func(time_func->get_rpSymbol())
+        , implement_func(implement_func->get_rpSymbol())
+        , pp_time_func(nullptr)
+        , pp_implement_func(nullptr)
+        , pp_event_id(0)
     {
     }
 
-//    void post_parse(int pass);
-
-    /** The Symbol for the time function of the event.*/
-//    Symbol*& time_function;
+    void post_parse(int pass);
 
 
     /**
-     * Numeric identifier for the event. The numeric identifier is the ordinal of the event name
-     * among all events in the model.  If two agents have an event with the same name, \a
-     * pp_event_id will be identical for those two events.
-     */
+    * Name of the event.
+    */
 
-//    int pp_event_id;
+    string event_name;
+
+    /**
+    * Gets the initial value for the data member
+    *
+    * Events are disabled at initialization by setting the event_time to time_infinite.
+    *
+    * @return The initial value as a string.
+    */
+
+    string initial_value() const
+    {
+        return token_to_string(token::TK_time_infinite);
+    }
+
+    CodeBlock cxx_declaration_agent();
+
+    /** The Symbol for the time function of the event.*/
+    Symbol*& time_func;
+
+    Symbol*& implement_func;
+
+    AgentFuncSymbol *pp_time_func;
+
+    AgentFuncSymbol *pp_implement_func;
+
+    /**
+    * Numeric identifier for the event. The numeric identifier is the ordinal of the event name
+    * among all events in the model.  If two agents have an event with the same name, \a
+    * pp_event_id will be identical for those two events.
+    */
+
+    int pp_event_id;
 };
 
