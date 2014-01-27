ed18b98c877f9671a6d5dc037b2f681da26b940f
esseffgee <esseffgee@users.sf.net>
2014-01-27 14:26:13 +0000

    omc: Rename BoolEnumTypeSymbol to BoolSymbol for design, code, and documentation clarity

---
+++ openm/omc/BoolSymbol.cpp
@@ -0,0 +1,31 @@
+/**
+* @file    BoolSymbol.cpp
+* Definitions for the BoolSymbol class.
+*/
+// Copyright (c) 2013-2014 OpenM++
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
+#include "BoolSymbol.h"
+#include "BoolEnumeratorSymbol.h"
+
+using namespace std;
+
+void BoolSymbol::post_parse(int pass)
+{
+    // Hook into the post_parse hierarchical calling chain
+    super::post_parse(pass);
+
+    // Perform post-parse operations specific to this level in the Symbol hierarchy.
+    switch (pass) {
+    case eCreateMissingSymbols:
+    {
+        // Create enumerators for false and true, associated with the bool enumeration symbol
+        auto sym0 = new BoolEnumeratorSymbol(token_to_string(token::TK_false), this, 0);
+        auto sym1 = new BoolEnumeratorSymbol(token_to_string(token::TK_true), this, 1);
+    }
+    break;
+    default:
+    break;
+    }
+}
+
---
+++ openm/omc/BoolSymbol.h
@@ -0,0 +1,37 @@
+/**
+* @file    BoolSymbol.h
+* Declarations for the BoolSymbol class.
+*/
+// Copyright (c) 2013-2014 OpenM++
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
+#pragma once
+#include "EnumTypeSymbol.h"
+#include "TypeSymbol.h"
+
+using namespace std;
+using namespace openm;
+
+/**
+* BoolSymbol.
+*/
+class BoolSymbol : public EnumTypeSymbol
+{
+private:
+    typedef EnumTypeSymbol super;
+
+public:
+    bool is_base_symbol() const { return false; }
+
+    /**
+    * Constructor for the bool enumeration symbol
+    */
+
+    BoolSymbol()
+        : EnumTypeSymbol(token_to_string(token::TK_bool), token::TK_bool, kind_of_type::logical_type)
+    {
+    }
+
+    void post_parse(int pass);
+};
+
