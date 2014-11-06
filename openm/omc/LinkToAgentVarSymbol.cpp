234760e516707f711a8b66ca11331c22389e1519
esseffgee <esseffgee@users.sf.net>
2014-11-06 15:02:25 +0000

    omc: bug fix: missing implementation for agentvars of one-to-many links (was present only for one-to-one links)

--- openm/omc/LinkToAgentVarSymbol.cpp
+++ openm/omc/LinkToAgentVarSymbol.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 #include "LinkToAgentVarSymbol.h"
 #include "LinkAgentVarSymbol.h"
+#include "AgentMultilinkSymbol.h"
 #include "AgentSymbol.h"
 #include "CodeBlock.h"
 
@@ -58,15 +59,28 @@ void LinkToAgentVarSymbol::post_parse(int pass)
 
         // assign direct pointer to link for use post-parse
         pp_link = dynamic_cast<LinkAgentVarSymbol *> (pp_symbol(link));
-        assert(pp_link); // TODO: possible model source code error
+        assert(pp_link); // parser guarantee (link statement)
 
         // assign direct pointer to agentvar in agent at other end of link
-        assert(pp_link->reciprocal_link); // TODO: possible model coding error?
-        assert(pp_link->reciprocal_link->agent); // TODO: possible model coding error?
-        auto sym = Symbol::get_symbol(agentvar, pp_link->reciprocal_link->agent);
-        assert(sym); // parser / scanner guarantee
+        Symbol *agent = nullptr;
+        if (pp_link->reciprocal_link) {
+            // is a one-to-one link
+            auto reciprocal = pp_link->reciprocal_link;
+            agent = reciprocal->agent;
+            assert(agent); // grammar guarantee (link statement)
+        }
+        else {
+            // is a one-to-many link
+            assert(pp_link->reciprocal_multilink); // grammar guarantee (must be either link or multilink)
+            auto reciprocal = pp_link->reciprocal_multilink;
+            agent = reciprocal->agent;
+            assert(agent); // grammar guarantee (link statement)
+        }
+        auto sym = Symbol::get_symbol(agentvar, agent);
         auto av = dynamic_cast<AgentVarSymbol *> (sym);
-        assert(av); // TODO possible model source code error
+        if (!av) {
+            pp_error("error - invalid member '" + agentvar + "' of link '" + pp_link->name + "'");
+        }
         pp_agentvar = av;
         break;
     }
