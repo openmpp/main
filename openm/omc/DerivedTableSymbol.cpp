761000fab39a9fb3b0d82310aef1a448352ad048
esseffgee <esseffgee@users.sf.net>
2015-05-13 03:18:42 +0000

    omc: derived tables: safety checkpoint

--- openm/omc/DerivedTableSymbol.cpp
+++ openm/omc/DerivedTableSymbol.cpp
@@ -6,10 +6,17 @@
 // This code is licensed under MIT license (see LICENSE.txt for details)
 
 #include <cassert>
+#include <regex>
 #include "DerivedTableSymbol.h"
+#include "LanguageSymbol.h"
 #include "EnumerationSymbol.h"
+#include "libopenm/db/metaModelHolder.h"
 
 using namespace std;
+using namespace openm;
+
+// this function exist only because g++ below 4.9 does not support std::regex
+static string regexReplace(const string & i_srcText, const char * i_pattern, const char * i_replaceWith);
 
 void DerivedTableSymbol::post_parse(int pass)
 {
@@ -51,101 +58,157 @@ void DerivedTableSymbol::populate_metadata(openm::MetaModelHolder & metaRows)
     // Hook into the hierarchical calling chain
     super::populate_metadata(metaRows);
 
-    //// Perform operations specific to this level in the Symbol hierarchy.
-    //TableDicRow tableDic;
+    // Perform operations specific to this level in the Symbol hierarchy.
+    TableDicRow tableDic;
 
-    //tableDic.tableId = pp_table_id;
-    //tableDic.tableName = name;
-    //tableDic.isUser = false;
-    //tableDic.rank = rank();
-    //tableDic.isSparse = true;   // do not store NULLs
-    //tableDic.isHidden = false;
-    //tableDic.exprPos = expr_dim_position;
-    //metaRows.tableDic.push_back(tableDic);
+    tableDic.tableId = pp_table_id;
+    tableDic.tableName = name;
+    tableDic.isUser = true;
+    tableDic.rank = rank();
+    tableDic.isSparse = true;   // do not store NULLs
+    tableDic.isHidden = false;
+    tableDic.exprPos = expr_dim_position;
+    metaRows.tableDic.push_back(tableDic);
 
-    //for (auto lang : Symbol::pp_all_languages) {
-    //    TableDicTxtLangRow tableTxt;
-    //    tableTxt.tableId = pp_table_id;
-    //    tableTxt.langName = lang->name;
-    //    tableTxt.descr = label(*lang);
-    //    tableTxt.note = note(*lang);
-    //    tableTxt.exprDescr = "Expressions (" + lang->name + ")"; // TODO
-    //    tableTxt.exprNote = "Expressions Note (" + lang->name + ")"; // TODO
-    //    metaRows.tableTxt.push_back(tableTxt);
-    //}
+    for (auto lang : Symbol::pp_all_languages) {
+        TableDicTxtLangRow tableTxt;
+        tableTxt.tableId = pp_table_id;
+        tableTxt.langName = lang->name;
+        tableTxt.descr = label(*lang);
+        tableTxt.note = note(*lang);
+        tableTxt.exprDescr = "Expressions (" + lang->name + ")"; // TODO
+        tableTxt.exprNote = "Expressions Note (" + lang->name + ")"; // TODO
+        metaRows.tableTxt.push_back(tableTxt);
+    }
 
-    //// dimensions for table
-    //for (auto dim : dimension_list ) {
-    //    auto av = dim->pp_attribute;
-    //    assert(av); // logic guarantee
-    //    auto es = dim->pp_enumeration;
-    //    assert(es); // logic guarantee
-    //    TableDimsRow tableDims;
-    //    tableDims.tableId = pp_table_id;
-    //    tableDims.dimId = dim->index;
-    //    //tableDims.name = "dim" + to_string(dim->index);
-    //    tableDims.typeId = es->type_id;
-    //    tableDims.isTotal = false;
-    //    tableDims.dimSize = es->pp_size();
-    //    metaRows.tableDims.push_back(tableDims);
+    // dimensions for table
+    for (auto dim : dimension_list ) {
+        auto es = dim->pp_enumeration;
+        assert(es); // logic guarantee
+        TableDimsRow tableDims;
+        tableDims.tableId = pp_table_id;
+        tableDims.dimId = dim->index;
+        //tableDims.name = "dim" + to_string(dim->index);
+        tableDims.typeId = es->type_id;
+        tableDims.isTotal = false;
+        tableDims.dimSize = es->pp_size();
+        metaRows.tableDims.push_back(tableDims);
 
-    //    for (auto lang : Symbol::pp_all_languages) {
-    //        TableDimsTxtLangRow tableDimsTxt;
-    //        tableDimsTxt.tableId = pp_table_id;
-    //        tableDimsTxt.dimId = dim->index;
-    //        tableDimsTxt.langName = lang->name;
-    //        tableDimsTxt.descr = dim->label(*lang);
-    //        tableDimsTxt.note = dim->note(*lang);
-    //        metaRows.tableDimsTxt.push_back(tableDimsTxt);
-    //    }
-    //}
+        for (auto lang : Symbol::pp_all_languages) {
+            TableDimsTxtLangRow tableDimsTxt;
+            tableDimsTxt.tableId = pp_table_id;
+            tableDimsTxt.dimId = dim->index;
+            tableDimsTxt.langName = lang->name;
+            tableDimsTxt.descr = dim->label(*lang);
+            tableDimsTxt.note = dim->note(*lang);
+            metaRows.tableDimsTxt.push_back(tableDimsTxt);
+        }
+    }
 
-    //// accumulators for table
-    //for (auto acc : pp_accumulators) {
-    //    TableAccRow tableAcc;
+    // 'accumulators' for derived table
+    for (auto acc : pp_placeholders) {
+        TableAccRow tableAcc;
 
-    //    tableAcc.tableId = pp_table_id;
-    //    tableAcc.accId = acc->index;
-    //    // tableAcc.name = "acc" + to_string(acc->index);
-    //    tableAcc.expr = acc->pretty_name();
-    //    metaRows.tableAcc.push_back(tableAcc);
+        tableAcc.tableId = pp_table_id;
+        tableAcc.accId = acc->index;
+        // tableAcc.name = "acc" + to_string(acc->index);
+        tableAcc.expr = acc->pretty_name();
+        metaRows.tableAcc.push_back(tableAcc);
 
-    //    for (auto lang : Symbol::pp_all_languages) {
-    //        TableAccTxtLangRow tableAccTxt;
-    //        tableAccTxt.tableId = pp_table_id;
-    //        tableAccTxt.accId = acc->index;
-    //        tableAccTxt.langName = lang->name;
-    //        tableAccTxt.descr = acc->label(*lang);
-    //        tableAccTxt.note = acc->note(*lang);
-    //        metaRows.tableAccTxt.push_back(tableAccTxt);
-    //    }
-    //}
+        for (auto lang : Symbol::pp_all_languages) {
+            TableAccTxtLangRow tableAccTxt;
+            tableAccTxt.tableId = pp_table_id;
+            tableAccTxt.accId = acc->index;
+            tableAccTxt.langName = lang->name;
+            tableAccTxt.descr = acc->label(*lang);
+            tableAccTxt.note = acc->note(*lang);
+            metaRows.tableAccTxt.push_back(tableAccTxt);
+        }
+    }
 
-    //// expressions for table
-    //for (auto expr : pp_expressions) {
-    //    TableExprRow tableExpr;
-    //    tableExpr.tableId = pp_table_id;
-    //    tableExpr.exprId = expr->index;
-    //    tableExpr.name = "expr" + to_string(expr->index);
-    //    tableExpr.src = expr->get_expression(expr->root, TableExpressionSymbol::expression_style::sql);
-    //    metaRows.tableExpr.push_back(tableExpr);
+    // 'expressions' for derived table
+    for (auto expr : pp_placeholders) {
+        TableExprRow tableExpr;
+        tableExpr.tableId = pp_table_id;
+        tableExpr.exprId = expr->index;
+        tableExpr.name = "expr" + to_string(expr->index);
+        tableExpr.src = "AVG(acc" + to_string(expr->index) + ")";
+        metaRows.tableExpr.push_back(tableExpr);
 
-    //    for (auto lang : Symbol::pp_all_languages) {
-    //        TableExprTxtLangRow tableExprTxt;
-    //        tableExprTxt.tableId = pp_table_id;
-    //        tableExprTxt.exprId = expr->index;
+        for (auto lang : Symbol::pp_all_languages) {
+            TableExprTxtLangRow tableExprTxt;
+            tableExprTxt.tableId = pp_table_id;
+            tableExprTxt.exprId = expr->index;
 
-    //        tableExprTxt.langName = lang->name;
+            tableExprTxt.langName = lang->name;
 
-    //        // construct label by removing decimals=nnn from string if present
-    //        tableExprTxt.descr = regexReplace(expr->label(*lang), "[[:space:]]+decimals=[[:digit:]]+", "");
-    //        
-    //        tableExprTxt.note = expr->note(*lang);
-    //        metaRows.tableExprTxt.push_back(tableExprTxt);
-    //    }
-    //}
+            // construct label by removing decimals=nnn from string if present
+            tableExprTxt.descr = regexReplace(expr->label(*lang), "[[:space:]]+decimals=[[:digit:]]+", "");
+            
+            tableExprTxt.note = expr->note(*lang);
+            metaRows.tableExprTxt.push_back(tableExprTxt);
+        }
+    }
 }
 
+// this function exist only because g++ below 4.9 does not support std::regex
+#ifdef _WIN32
+
+string regexReplace(const string & i_srcText, const char * i_pattern, const char * i_replaceWith)
+{
+    regex pat(i_pattern);
+    return regex_replace(i_srcText, pat, i_replaceWith);
+}
+
+#else
+
+#include <regex.h>
+
+#define MAX_RE_ERROR_MSG    1024
+
+string regexReplace(const string & i_srcText, const char * i_pattern, const char * i_replaceWith)
+{
+    // prepare regex
+    regex_t re;
+
+    int nRet = regcomp(&re, i_pattern, REG_EXTENDED | REG_NEWLINE);
+    if (nRet != 0) {
+        char errMsg[MAX_RE_ERROR_MSG + 1];
+        regerror(nRet, &re, errMsg, sizeof(errMsg));
+        throw HelperException("Regular expression error: %s", errMsg);
+    }
+
+    // replace first occurrence of pattern in source text
+    string sResult;
+    regmatch_t matchPos;
+
+    nRet = regexec(&re, i_srcText.c_str(), 1, &matchPos, 0);
+    if (nRet == REG_NOMATCH) {
+        regfree(&re);           // cleanup
+        return i_srcText;       // pattern not found - return source text as is
+    }
+    if (nRet != 0) {            // error 
+        regfree(&re);           // cleanup
+        throw HelperException("Regular expression FAILED: %s", i_pattern);
+    }
+    // at this point nRet == 0 and we can replace first occurrence with replacement string
+    if (matchPos.rm_so >= 0 && matchPos.rm_so < (int)i_srcText.length()) {
+
+        string sResult =
+            i_srcText.substr(0, matchPos.rm_so) +
+            i_replaceWith +
+            ((matchPos.rm_eo >= 0 && matchPos.rm_eo < (int)i_srcText.length()) ? i_srcText.substr(matchPos.rm_eo) : "");
+
+        regfree(&re);       // cleanup
+        return sResult;
+    }
+    else {                  // starting offset out of range - pattern not found
+        regfree(&re);       // cleanup
+        return i_srcText;   // return source text as is
+    }
+}
+
+#endif // _WIN32
 
 
 
