9d4846bd4b010735c24adc6989fd566ee1fb6826
esseffgee <esseffgee@users.sf.net>
2015-06-06 19:59:33 +0000

    models: TableProbe1: Special purpose model to test correctness of table implementation

---
+++ models/TableProbe1/code/custom.h
@@ -0,0 +1,11 @@
+/**
+ * @file   custom.h
+ * Custom include file for the model
+ *  
+ * This file is included in all compiler-generated files for the model.
+ * Place any required globals declarations in this file, or include other
+ * files in this file, for correct placement in compiler-generated files.
+ */
+
+extern void signal_exit_simulation_all();
+
