a8f8ea4a2f33a65663a93c48ad805204012b3d72
esseffgee <esseffgee@users.sf.net>
2015-08-05 13:21:04 +0000

    deploy: Add selected test models (Alphs1, RiskPaths) to Windows deployment image script deploy_windows.pl

--- Perl/deploy_windows.pl
+++ Perl/deploy_windows.pl
@@ -3,6 +3,8 @@
 
 # Script to prepare files for binary deployment
 
+# SHould be re-factored to use subroutines rather than all that repetitve copying...
+
 # Check for and process -no64 flag
 my $no64 = 0;
 if ($#ARGV == 0 && $ARGV[0] eq '-no64') {
@@ -29,13 +31,14 @@ chdir "..";
 use Cwd qw(getcwd);
 my $om_root = getcwd;
 
-for my $subdir ('bin', 'include', 'lib', 'props', 'R', 'sql', 'use') {
-	-d "${om_root}/${subdir}" or die;
+# Preliminary check of source folder structure
+for my $subdir ('bin', 'include', 'lib', 'props', 'R', 'sql', 'use', 'models') {
+	-d "${om_root}/${subdir}" or die "Missing directory ${om_root}/${subdir}";
 }
 
 my $deploy_dir = "deploy_windows";
 remove_tree $deploy_dir;
-mkdir $deploy_dir || die;
+mkdir $deploy_dir or die "Unable to remove deployment directory ${deploy_dir}";
 
 my $subdir;
 my @files;
@@ -53,10 +56,6 @@ mkdir "${deploy_dir}/${subdir}" or die;
 for my $file (@files) {
 	copy "${om_root}/${subdir}/${file}", "${deploy_dir}/${subdir}/${file}" or die "Failed to copy ${subdir}/${file}";
 }
-
-# include
-$subdir = 'include';
-dircopy $subdir, "${deploy_dir}/${subdir}" || die;
 
 # lib
 $subdir = 'lib';
@@ -78,6 +77,10 @@ for my $file (@files) {
 	copy "${om_root}/${subdir}/${file}", "${deploy_dir}/${subdir}/${file}" or die "Failed to copy ${subdir}/${file}";
 }
 
+# include
+$subdir = 'include';
+dircopy $subdir, "${deploy_dir}/${subdir}" || die;
+
 # props
 $subdir = 'props';
 dircopy $subdir, "${deploy_dir}/${subdir}" || die;
@@ -94,4 +97,54 @@ dircopy $subdir, "${deploy_dir}/${subdir}" || die;
 $subdir = 'use';
 dircopy $subdir, "${deploy_dir}/${subdir}" || die;
 
+# models
+mkdir "${deploy_dir}/models" or die;
 
+$subdir = 'models';
+@files = (
+	'.gitignore',
+	);
+for my $file (@files) {
+	copy "${om_root}/${subdir}/${file}", "${deploy_dir}/${subdir}/${file}" or die "Failed to copy ${subdir}/${file}";
+}
+
+# Selected models
+@models = (
+	'Alpha1',
+	'RiskPaths',
+	);
+for my $model (@models) {
+	my $model_dir = "models/${model}";
+	mkdir "${deploy_dir}/${model_dir}" or die;
+	
+	# code
+	$subdir = "${model_dir}/code";
+	dircopy $subdir, "${deploy_dir}/${subdir}" || die;
+	
+	# parameters
+	$subdir = "${model_dir}/parameters";
+	dircopy $subdir, "${deploy_dir}/${subdir}" || die;
+	
+	# solution files
+	$subdir = "${model_dir}";
+	my @files = glob("${subdir}/*.sln");
+	for my $file (@files) {
+		copy "${om_root}/${file}", "${deploy_dir}/${subdir}" or die "Failed to copy ${subdir}/${file}";
+	}
+	
+	# modgen project files
+	$subdir = "${model_dir}/modgen";
+	mkdir "${deploy_dir}/${subdir}" or die;
+	my @files = glob("${subdir}/*.vcxproj ${subdir}/*.vcxproj.filters ${subdir}/*.props");
+	for my $file (@files) {
+		copy "${om_root}/${file}", "${deploy_dir}/${subdir}" or die "Failed to copy ${subdir}/${file}";
+	}
+	
+	# ompp project files
+	$subdir = "${model_dir}/ompp";
+	mkdir "${deploy_dir}/${subdir}" or die;
+	my @files = glob("${subdir}/*.vcxproj ${subdir}/*.vcxproj.filters ${subdir}/*.props");
+	for my $file (@files) {
+		copy "${om_root}/${file}", "${deploy_dir}/${subdir}" or die "Failed to copy ${subdir}/${file}";
+	}
+}
