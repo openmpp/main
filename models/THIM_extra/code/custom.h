4700ee84f3fb979cb276bd03550cd8a0323d3463
Steve Gribble <Steve.Gribble@hotmail.com>
2016-02-11 14:46:29 -0500

    Distinguish release models from models with extra code for testing - e.g. RiskPaths_extra Comment change in NewTimeBased - don't illustrate deprecated Modgen option index_type

---
+++ models/THIM_extra/code/custom.h
@@ -0,0 +1,10 @@
+/**
+ * @file   custom.h
+ * Custom include file for the model
+ *  
+ * This file is included in all compiler-generated files for the model.
+ * Place any required globals declarations in this file, or include other
+ * files in this file, for correct placement in compiler-generated files.
+ */
+
+#include <cassert>
