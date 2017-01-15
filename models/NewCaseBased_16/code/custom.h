f9f6333ed02d2623b315b2f07b08ab24b526bb5d
Steve Gribble <Steve.Gribble@hotmail.com>
2017-01-15 17:28:30 -0500

    NewCaseBased_16 - version of NewCaseBased with 16 members and multithreading for automated testing

---
+++ models/NewCaseBased_16/code/custom.h
@@ -0,0 +1,10 @@
+/**
+ * @file   custom.h
+ * Model-specific declarations and includes (late)
+ *  
+ * This file is included in all compiler-generated files for the model.
+ * This file comes late in the include order, after entity declarations.
+ */
+
+#pragma once
+#include "case_based.h"
