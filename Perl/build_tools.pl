92e5bfa5dbe574f4288fdc3c3bde7141e94896dc
amc1999 <amc1999@gmail.com>
2017-05-03 20:09:16 -0400

    upgrade projects and perl scripts to VS2017

--- Perl/build_tools.pl
+++ Perl/build_tools.pl
@@ -3,7 +3,7 @@
 
 # Script to build utilities, etc.
 
-my $R_exe = 'C:\Program Files\R\R-3.2.3\bin\R.exe';
+my $R_exe = 'C:\Program Files\R\R-3.4.0\bin\R.exe';
 
 # Verbosity of report:
 #  0 - errors only
