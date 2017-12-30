6f7372cabd7f7a136eeb7a2352d619b9ccfa2fe3
Steve Gribble <Steve.Gribble@hotmail.com>
2017-12-29 21:30:25 -0500

    implementation of min,max,clamp templates

---
+++ include/omc/min_max.h
@@ -0,0 +1,41 @@
+/**
+* @file    min_max.h
+* Implementation of min, max, clamp templates
+*/
+// Copyright (c) 2013-2016 OpenM++
+// This code is licensed under the MIT license (see LICENSE.txt for details)
+
+#include <type_traits>
+
+// undefine min and max macros if defined (enables use in Modgen models)
+#ifdef min
+#undef min
+#endif
+#ifdef max
+#undef max
+#endif
+
+// Replacement versions in global namespace of std:min and std::max
+// which enable type conversion of arguments to a common shared type.
+// Specializations of common_type for ompp types enable their particpation.
+template <typename T, typename U, typename CT = typename std::common_type<T, U>::type>
+inline CT min(T a, U b)
+{
+    return (CT)a < (CT)b ? (CT)a : (CT)b;
+}
+
+template <typename T, typename U, typename CT = typename std::common_type<T, U>::type>
+inline CT max(T a, U b)
+{
+    return (CT)a < (CT)b ? (CT)b : (CT)a;
+}
+
+// Replacement versions in global namespace of std:min and std::max
+// which enable type conversion of arguments to a common shared type.
+template <typename T, typename U, typename V, typename CT = typename std::common_type<T, U, V>::type>
+inline CT clamp(T inf, U sup, V x)
+{
+    if ((CT)inf >(CT)x) return (CT)inf;
+    else if ((CT)sup < (CT)x) return (CT)sup;
+    else return (CT)x;
+}
