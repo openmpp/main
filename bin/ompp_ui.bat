770115cc0f7155b5c4796f80da97b69f1af0afba
amc1999 <29126882+amc1999@users.noreply.github.com>
2021-01-14 23:22:07 -0500

    start UI batch: display error message if models\bin not exist

--- bin/ompp_ui.bat
+++ bin/ompp_ui.bat
@@ -6,8 +6,12 @@ IF "%CD%\" == "%~dp0" (
 IF "%OM_ROOT%" == "" (
   SET OM_ROOT=%CD%
 )
-rem cd %OM_ROOT%
+
+if not exist %OM_ROOT%\models\bin (
+  @echo ERROR: missing models bin directory: %OM_ROOT%\models\bin
+  PAUSE
+  EXIT 1
+)
 
 START "oms" /MIN bin\oms
 START http://localhost:4040
-
