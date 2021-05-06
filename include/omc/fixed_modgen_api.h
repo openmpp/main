df68729e428d0ee605c3637b090916aa64e63bb3
esseff <steve.gribble@hotmail.com>
2021-05-06 16:29:29 -0400

    Bug fix in Modgen cover macros FixedGetTableValue and FixedSetTableValue

--- include/omc/fixed_modgen_api.h
+++ include/omc/fixed_modgen_api.h
@@ -24,17 +24,28 @@ inline double NaN_to_UNDEF_VALUE(double x)
     return (std::isnan(x)) ? UNDEF_VALUE : x;
 }
 
-#define FixedGetTableValue(table_cell, ...) \
-    UNDEF_VALUE_to_NaN(GetTableValue(table_cell, ##__VA_ARGS__))
-#define FixedSetTableValue(table_cell, value, ...) \
-    SetTableValue(table_cell, NaN_to_UNDEF_VALUE(value), ##__VA_ARGS__)
+// Obscurely, Modgen inserts a trailing -1 argument in marked up code,
+// which explains why it is also inserted here by the cover macros.
+// The following macros use compiler specific behaviour,
+// but in any case only apply to Modgen compilation under MSVC.
+// The ## operator removes the preceding , if no arguments are supplied, but is non-standard.
+
+#define FixedGetTableValue(table_measure, ...) \
+    UNDEF_VALUE_to_NaN(GetTableValue(table_measure, ## __VA_ARGS__, -1))
+
+#define FixedSetTableValue(table_measure, value, ...) \
+    SetTableValue(table_measure, NaN_to_UNDEF_VALUE(value), ## __VA_ARGS__, -1)
 
 #else // defined(OPENM)
 
-#define FixedGetTableValue(table_cell, ...) \
-    GetTableValue(table_cell, ##__VA_ARGS__)
-#define FixedSetTableValue(table_cell, value, ...) \
-    SetTableValue(table_cell, value, ##__VA_ARGS__)
+// The use of ## to suppress the preceding comma is non-standard.
+// It should be reworked to use the standards compliant __VA_OPT__ mechanism in C++20
+// (although MSVC turns off the standards compliant preprocessor by default 2021-05-06).
+// OTOH, this whole derived table mechanism in Modgen should be replaced.
+#define FixedGetTableValue(table_measure, ...) \
+    GetTableValue(table_measure, ##__VA_ARGS__)
+#define FixedSetTableValue(table_measure, value, ...) \
+    SetTableValue(table_measure, value, ##__VA_ARGS__)
 
 #endif
 
