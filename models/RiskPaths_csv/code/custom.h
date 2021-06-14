fc447dfa0b7a5195eab15ceb8f41abd906af8f8b
esseff <steve.gribble@hotmail.com>
2021-06-14 07:53:31 -0400

    Starting version of new model RiskPaths_csv for csv testing, routine or otherwise

---
+++ models/RiskPaths_csv/code/custom.h
@@ -0,0 +1,11 @@
+/**
+ * @file   custom.h
+ * Model-specific declarations and includes (late)
+ *  
+ * This file is included in all compiler-generated files for the model.
+ * This file comes late in the include order, after entity declarations.
+ */
+
+#pragma once
+#include "case_based.h"
+#include "omc/fixed_modgen_api.h"
