c94a77c2e8fb824f1c1f179342e4efcda5d5d5eb
esseff <steve.gribble@hotmail.com>
2021-12-06 09:40:50 -0500

    Add new model NewCaseBased_weighted to test suite to test weighted tables implementation

---
+++ models/NewCaseBased_weighted/code/custom.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file   custom.cpp
+ * Custom C++ file for the model
+ * 
+ * This file is compiled and linked into the model.
+ * Place global definitions (if any) in this file.
+ * All global definitions must be thread-safe.
+ */
+
+// globals
