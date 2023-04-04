a1a452f37cf3f4152726399856e0d8e570c20866
amc1999 <amc1999@gmail.com>
2023-04-04 16:44:25 -0400

    changes in comments only

--- include/libopenm/common/omOS.h
+++ include/libopenm/common/omOS.h
@@ -13,7 +13,18 @@
 
 namespace openm
 {
-    /* return current process memory size and max peak memory size, in bytes. */
+    /** return current process memory size and max peak memory size, in bytes.
+    *
+    * if return value is zero then memory information not avaliable.
+    * 
+    * usage example: \n
+    * @code
+    *      auto [nCurrentMem, nMaxMem] getProcessMemorySize();
+    * 
+    *      if (nCurrentMem > 0) theLog->logFormatted("Current process memory: %llu bytes", nCurrentMem);
+    *      if (nMaxMem > 0) theLog->logFormatted("Maximum process memory: %llu bytes", nMaxMem);
+    * @endcode
+    */
     extern std::tuple<uint64_t, uint64_t> getProcessMemorySize(void);
 }
 
