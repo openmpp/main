6f9acefd341b6522a559739abec81f5c43965044
esseff <steve.gribble@hotmail.com>
2024-06-02 20:34:48 -0400

    omc - model code markup - implementation of index_errors - multidimensional parameters (WIP)

--- openm/omc/omc_markup.cpp
+++ openm/omc/omc_markup.cpp
@@ -28,38 +28,72 @@ void do_markup(const string& om_developer_cpp_path)
 
     theLog->logMsg("Model code markup - start");
 
-    /// patterns and replacements
+    /// vector of pattern and replacement pairs
     std::vector<std::pair<std::regex, std::string>> patterns;
     {
-        // populate patterns and replacements
-
-        // Tests using RiskPaths
-        //patterns.push_back({ std::regex("AgeBaselinePreg1\\["), "AgeBaselinePreg1[0+" });
-        //patterns.push_back({ std::regex("AgeBaselinePreg1\\[([a-z_]+)\\]"), "AgeBaselinePreg1[0+$1]" });
-        //patterns.push_back({ std::regex("(AgeBaselinePreg1)\\[([^\\]]+)\\]"), "$1[($2)]" });
-        //patterns.push_back({ std::regex("(\\bAgeBaselinePreg1\\b)\\[([^\\]]+)\\]"), "$1[($2)]" });
-        //patterns.push_back({ std::regex("(\\bAgeBaselinePreg1\\b\\s*)\\[([^\\]]+)\\]"), "$1[($2)]" });
-        //patterns.push_back({ std::regex("(\\bAgeBaselinePreg1\\b)(\\s*)\\[([^\\]]+)\\]"), "$1$2[om_check_index($3,99,0,\"$1\")]" });
+        // populate regex pattern and replacement pairs
         for (auto p : Symbol::pp_all_parameters) {
-            if (p->rank() == 1) {  // TODO extend to multiple dimensions
-                // ex: (\bAgeBaselinePreg1\b)(\s*)\[([^\[\]]+)\]
-                /// Regex matching a symbol name followed by indices (one for each dimension)
-                string srch;
-                srch += "(\\b" + p->name + "\\b)";  // group 1: the symbol name. \b is 0-length positional identifying begin or end boundary of a 'word'
-                srch += "(\\s*)";                   // group 2: optional white space following the symbol name
-                srch += "\\[";                      // left bracket
-                srch += "([^\\[\\]]+)";             // group 3: everything inside brackets (except [, to ignore nested constructs)
-                srch += "\\]";                      // right bracket
-                // ex: $1$2[om_check_index($3,99,0,"$1")]
-                /// The replacement string for regex
-                string repl;
-                /// The 0-based dimension number of this index
-                size_t dim = 0;
-                /// The size of this dimension
-                size_t size = p->pp_shape.front();
-                repl += "$1$2[om_check_index($3, " + to_string(size) + ", " + to_string(dim) + ", \"$1\", __FILE__, __LINE__)]";
-                patterns.push_back({ std::regex(srch), repl });
+            if (p->rank() == 0) {
+                // no index checking for scalars!
+                continue;
             }
+
+            //if (p->name != "UnionDurationBaseline") continue; // testing
+
+            /// Regex matching a symbol name followed by indices (one bracketed index expression for each dimension)
+            string srch = "";
+            // ex: (\bAgeBaselinePreg1\b)(\s*)\[([^\[\]]+)\]
+
+            /// The replacement string for regex
+            string repl = "";
+            // ex: $1$2[om_check_index($3,99,0,"$1")]
+
+            srch += "(\\b" + p->name + "\\b)";  // group 1: the symbol name. \b is 0-length positional identifying begin or end boundary of a 'word'
+            // ex: (\bAgeBaselinePreg1\b)
+
+            repl += "$1";
+            // ex: $1
+
+            int dim = 0;
+            int grp_white = 2;
+            int grp_index = 3;
+            for (auto dim_size : p->pp_shape) {
+                srch +=
+                    "(\\s*)"        // group $grp_white: optional white space
+                    // ex: (\s*)
+                    "\\["           // left bracket
+                    "([^\\[\\]]+)"  // group $grp_index: everything inside brackets (except [, to not match nested constructs)
+                    "\\]"           // right bracket
+                    // ex: \[([^\[\]]+)\]
+                    ;
+
+                repl += 
+                    "$" + to_string(grp_white) +
+                    // ex. $2
+                    "[om_check_index($" + to_string(grp_index) + "," +
+                    // ex. [om_check_index($3,
+                    to_string(dim_size) + "," + 
+                    // ex. 42,
+                    to_string(dim) + ","
+                    // ex. 0,
+                    "\"$1\","
+                    // ex. "$1",
+                    "__FILE__,__LINE__)]"
+                    // ex. __FILE__, __LINE__)]
+                    ;
+                // ex. $2[om_check_index($3,42,0,"$1",__FILE__,__LINE__)]
+                // $2 is possible whitespace preceding [...]
+                // $3 is the index expression
+                // 42 is the size of this dimension
+                // 0 is the 0-based dimension number
+                // $1 is the symbol (array) name
+                // advance dimension for next dimenion
+                ++dim;
+                // advance group counters for next dimension
+                grp_white += 2;
+                grp_index += 2;
+            }
+            patterns.push_back({ std::regex(srch), repl });
         }
     }
 
@@ -86,11 +120,17 @@ void do_markup(const string& om_developer_cpp_path)
 
     // create code_out from code_in
     code_out = code_in;
+    int counter = 1;
     for (auto& it : patterns) {
         // apply all patterns to model code
         auto srch = it.first;
         auto repl = it.second;
         code_out = std::regex_replace(code_out, srch, repl);
+        ++counter;
+        if (0 == counter % 100) {
+            // progress indicator if 100 or more patterns
+            theLog->logFormatted(" Pattern %d of %d", counter, patterns.size());
+        }
     }
 
     {
