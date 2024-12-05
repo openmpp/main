1b6410fbc9b5d5449b6c19096f0ea2a522ffd9b4
esseff <steve.gribble@hotmail.com>
2024-12-05 11:07:36 -0500

    attribute groups - move AC commented prototype of populate_metadata from EntitySymbol.cpp to AttributeGroupSymbol.cpp

--- openm/omc/AttributeGroupSymbol.cpp
+++ openm/omc/AttributeGroupSymbol.cpp
@@ -30,3 +30,80 @@ void AttributeGroupSymbol::populate_metadata(openm::MetaModelHolder& metaRows)
     // Perform operations specific to this level in the Symbol hierarchy.
 
 }
+
+/*
+* Prototype of
+*   populate_metadata()
+* for entity attribute groups
+*
+* Entity attribute groups are similar to parameters and output tables groups
+*
+void EntityAttributeGroupSymbol::populate_metadata(openm::MetaModelHolder& metaRows)
+{
+    using namespace openm;
+
+    // Hook into the hierarchical calling chain
+    super::populate_metadata(metaRows);
+
+    // Perform operations specific to this level in the Symbol hierarchy.
+
+    // groups of entity attributes
+    EntityGroupLstRow groupRow;
+
+    // basic information about the group
+    groupRow.entityId = pp_entity_id;
+    groupRow.groupId = pp_group_id;
+    groupRow.name = name;
+    groupRow.isHidden = is_hidden;
+    metaRows.entityGroupLst.push_back(groupRow);
+
+    // labels and notes for the group
+    for (const auto & langSym : Symbol::pp_all_languages) {
+        const string & lang = langSym->name; // e.g. "EN" or "FR"
+        EntityGroupTxtLangRow groupTxt;
+        groupTxt.entityId = pp_entity_id;
+        groupTxt.groupId = pp_group_id;
+        groupTxt.langCode = lang;
+        groupTxt.descr = label(*langSym);
+        groupTxt.note = note(*langSym);
+        metaRows.entityGroupTxt.push_back(groupTxt);
+    }
+
+    // group children
+    int childPos = 1;   // child position in the group, must be unique
+
+    for (auto sym : pp_symbol_list) {
+        auto ags = dynamic_cast<EntityAttributeGroupSymbol *>(sym);
+        if (ags) {
+            EntityGroupPcRow groupPc;
+            groupPc.entityId = pp_entity_id;
+            groupPc.groupId = pp_group_id;
+            groupPc.childPos = childPos++;
+            groupPc.childGroupId = ags->pp_group_id;
+            groupPc.attrId = -1;            // negative value treated as db-NULL
+            metaRows.entityGroupPc.push_back(groupPc);
+
+            continue;   // done with this child group
+        }
+        // else symbol is an entity attribute
+        auto attr = dynamic_cast<AttributeSymbol *>(sym);
+        if (attr) {
+            EntityGroupPcRow groupPc;
+            groupPc.entityId = pp_entity_id;
+            groupPc.groupId = pp_group_id;
+            groupPc.childPos = childPos++;
+            groupPc.childGroupId = -1;      // negative value treated as db-NULL
+            groupPc.attrId = attr->pp_attribute_id;
+            metaRows.entityGroupPc.push_back(groupPc);
+
+            continue;   // done with this child attribute
+        }
+        else {
+            // invalid entity attribute group member
+            // (previously detected error condition)
+        }
+    }
+}
+
+*/
+
